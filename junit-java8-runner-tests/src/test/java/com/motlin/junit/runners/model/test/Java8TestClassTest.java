8932edc6b0868949d424655de3b7c5e98f8e756b
Craig P. Motlin <craig@motlin.com>
2013-11-11 22:21:45 -0500

    Initial import.

---
+++ junit-java8-runner-tests/src/test/java/com/motlin/junit/runners/model/test/Java8TestClassTest.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright 2013 Craig Motlin.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.motlin.junit.runners.model.test;
+
+import com.motlin.junit.runners.Java8Runner;
+import com.motlin.junit.runners.model.Java8TestClass;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.model.FrameworkMethod;
+
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+public class Java8TestClassTest {
+
+    public interface Java8SuperInterface {
+        @Test
+        default void superTest() {
+            Assert.assertTrue(true);
+        }
+    }
+
+    public interface Java8SubInterface extends Java8SuperInterface {
+        @Test
+        default void subTest() {
+            Assert.assertTrue(true);
+        }
+    }
+
+    @RunWith(Java8Runner.class)
+    public static class Java8Class implements Java8SubInterface {
+    }
+
+    @Test
+    public void findDefaultMethodTests()
+    {
+        Java8TestClass testClass = new Java8TestClass(Java8Class.class);
+        List<FrameworkMethod> methods = testClass.getAnnotatedMethods(Test.class);
+        assertThat(methods.size(), is(2));
+        assertThat(methods.get(0).getName(), is("subTest"));
+        assertThat(methods.get(1).getName(), is("superTest"));
+    }
+}
---
+++ junit-java8-runner/src/main/java/com/motlin/junit/runners/model/Java8TestClass.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright 2013 Craig Motlin.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.motlin.junit.runners.model;
+
+import org.junit.internal.MethodSorter;
+import org.junit.runners.model.FrameworkField;
+import org.junit.runners.model.FrameworkMethod;
+import org.junit.runners.model.TestClass;
+
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+import java.util.List;
+import java.util.Map;
+
+public class Java8TestClass extends TestClass {
+    /**
+     * Creates a {@code TestClass} wrapping {@code klass}. Each time this
+     * constructor executes, the class is scanned for annotations, which can be
+     * an expensive process (we hope in future JDK's it will not be.) Therefore,
+     * try to share instances of {@code TestClass} where possible.
+     *
+     * @param klass
+     */
+    public Java8TestClass(Class<?> klass) {
+        super(klass);
+    }
+
+    @Override
+    protected void scanAnnotatedMembers(Map<Class<? extends Annotation>, List<FrameworkMethod>> methodsForAnnotations, Map<Class<? extends Annotation>, List<FrameworkField>> fieldsForAnnotations) {
+        super.scanAnnotatedMembers(methodsForAnnotations, fieldsForAnnotations);
+
+        if (fClass!=null) {
+            getInterfaceMethodsForAnnotations(methodsForAnnotations, fClass);
+        }
+    }
+
+    private void getInterfaceMethodsForAnnotations(Map<Class<? extends Annotation>, List<FrameworkMethod>> methodsForAnnotations, Class<?> clazz)
+    {
+        Class<?>[] interfaces = clazz.getInterfaces();
+        for (Class<?> eachInterface : interfaces) {
+            for (Method eachMethod : MethodSorter.getDeclaredMethods(eachInterface)) {
+                if (!Modifier.isAbstract(eachMethod.getModifiers())) {
+                    addToAnnotationLists(new FrameworkMethod(eachMethod), methodsForAnnotations);
+                }
+            }
+        }
+        for (Class<?> eachInterface : interfaces) {
+            getInterfaceMethodsForAnnotations(methodsForAnnotations, eachInterface);
+        }
+    }
+
+}
